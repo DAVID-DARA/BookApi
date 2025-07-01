c782cef9ed4fa1d3d91c5a91353555b22840f2f2
Dara <davidfamoyegun@gmail.com>
2025-07-01 10:34:10 +0100

    Initialize Project

---
+++ src/main/java/com/crud/BookApiApplication.java
@@ -0,0 +1,17 @@
+package com.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class BookApiApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(BookApiApplication.class, args);
+    }
+}
